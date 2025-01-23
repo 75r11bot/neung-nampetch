2baea9b7a9f69b82f2ed98a3e73fa35b81af2700
n@ss <sendgridt7@gmail.com>
2025-01-24 00:30:19 +0700

    index update commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -37,7 +37,7 @@ $('.slick_slider_nav').slick({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(16.0668488, 102.7313426),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
